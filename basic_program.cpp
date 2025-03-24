0573e30c0f3a123ec40ca248e7a59b7f12fe0cc7
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-03-24 15:23:19 +0600

    add fibonacci

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int factorial(int n) {
     if (n <= 1) return 1;
     return n * factorial(n - 1);
 }
-
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 int main() {
     cout << "Hello World!" << endl;
     cout << "Factorial of 5: " << factorial(5) << endl;
+    cout << "Fibonacci of 6: " << fibonacci(6) << endl;
     return 0;
 }
