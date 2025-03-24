22257cca833aacac77db4d5c7de2d5c141564555
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-03-24 15:07:59 +0600

    Cherry pick factorial and nth prime function added

--- basic_program.cpp
+++ basic_program.cpp
@@ -4,13 +4,23 @@ int factorial(int n) {
     if (n <= 1) return 1;
     return n * factorial(n - 1);
 }
-int fibonacci(int n) {
-    if (n <= 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+int nthPrime(int n) {
+    int count = 0, num = 2;
+    while (count < n) {
+        if (isPrime(num)) count++;
+        if (count < n) num++;
+    }
+    return num;
 }
 int main() {
-    cout << "Hello World!" << endl;
-    cout << "Factorial of 5: " << factorial(5) << endl;
-    cout << "Fibonacci of 6: " << fibonacci(6) << endl;
+    cout << "Factorial of 5: " << factorial(5) << std::endl;
+    cout << "5th Prime: " << nthPrime(5) << std::endl;
     return 0;
 }
